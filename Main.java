ec920c0fd85db3ec9d049f70a36e7a4d74c5baa1
LGordy20 <liam.gordy@patelhs.org>
2021-02-04 14:13:20 +0000

    Started new method

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Hello what game are you looking for");
@@ -10,5 +11,13 @@ class Main {
     System.out.println("For 10 dollars you can get Rocket League");
     scan.next();
     System.out.println("Your welcome");
+
+    
+    static String greeting(String name);
+    {
+      String hello = "Hello again. What brings you back";
+      return hello;
+    }
+  
   }
 }
