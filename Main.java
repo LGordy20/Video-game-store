a9ce353d427df7174aa6d6ef662167929c8e042d
LGordy20 <liam.gordy@patelhs.org>
2021-02-04 13:38:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Hello what game are you looking for");
+    scan.next();
+    System.out.println("Out top selling games are Rocket Leaue, Forza and Call of Duty");
+    scan.next();
+    System.out.println("For 10 dollars you can get Rocket League");
+    scan.next();
+    System.out.println("Your Welcome");
+  }
+}
